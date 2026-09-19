7e8b49ef9136e974000bed69f6194f77045ba13a
YuriiSuperProg <uravasilev520@gmail.com>
2026-09-19 11:28:14 +0300

    learn dart syntax and types

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,5 +1,43 @@
 import 'package:lab1_dart/lab1_dart.dart' as lab1_dart;
 
 void main(List<String> arguments) {
-  print('Hello world: ${lab1_dart.calculate()}!');
+  // String name = "Артем";
+  // int age = 20;
+  // double height = 1.75;
+  // bool isStudent = true;
+  // print(name);
+  // print(age);
+  // print(height);
+  // print(isStudent);
+  // print("Привет, $name! Через тебе $age лет.");
+  // print("Через 5 лет тебе будет ${age + 5} лет.");
+  // print("Рост: $height м, студент: $isStudent");
+  // var score = 95;
+  // var language = "Dart";
+  // print("$language: $score");
+  // const String appName = "Lab1";
+  // final int startYear = 2026;
+  // print("$appName started in $startYear");
+  // String? city = null;
+  // if (city != null) {
+  //   print(city.toUpperCase());
+  // }
+  // print(city?.toUpperCase());
+  // List<String> fruits = ['яблоко', ',банан', 'груша'];
+  // fruits.add("Апельсин");
+  // print(fruits[0]);
+  // print(fruits.length);
+  // Map<String, dynamic> person = {"name": "Артем", "age": 20};
+  // print(person["name"]);
+  // person["city"] = "Волжский";
+  // Set<int> ids = {1, 2, 3, 2, 1};
+  // print(ids);
+  // print(ids.length);
+  List<String> fruits2 = ['яблоко', 'банан', 'груша'];
+  for (var fruit in fruits2) {
+    print(fruit);
+  }
+
+
+
 }
