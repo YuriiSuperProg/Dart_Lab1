8eb25657792b0944286828fc28e0ad9f6ebd82e4
YuriiSuperProg <uravasilev520@gmail.com>
2026-09-19 18:17:06 +0300

    learn dart functions

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -1,6 +1,37 @@
 import 'package:lab1_dart/lab1_dart.dart' as lab1_dart;
 
+// String greet(String name){
+//   return "Привет, $name";
+// }
+
+// int square(int x) => x * x;
+
+// double half(double x) => x / 2;
+
+// void describePet({required String name, String species = "кот", int age = 0}) {
+//   print("$name - $species, возраст $age");
+// }
+
+// String repeat(String text, [int times = 2]) {
+//   String result = '';
+//   for (int i = 0; i < times; i++){
+//     result += text;
+//   }
+//   return result;
+// }
+
+
 void main(List<String> arguments) {
+  // print(repeat("ха"));
+  // print(repeat("ха", 3));
+  // print(repeat("привет", 5));
+  // describePet(name: "Барсик", age: 3);
+  // describePet(name: "Шарик", species: "Пес", age: 5);
+  // describePet(name: "Карасик", species: "Рыба", age: 7);
+  // print(greet("Артем"));
+  // print(greet("Мария"));
+  // print(square(2));
+  // print(half(2.5));
   // String name = "Артем";
   // int age = 20;
   // double height = 1.75;
@@ -33,11 +64,16 @@ void main(List<String> arguments) {
   // Set<int> ids = {1, 2, 3, 2, 1};
   // print(ids);
   // print(ids.length);
-  List<String> fruits2 = ['яблоко', 'банан', 'груша'];
-  for (var fruit in fruits2) {
-    print(fruit);
-  }
-
-
-
+  // List<String> fruits2 = ['яблоко', 'банан', 'груша'];
+  // for (var fruit in fruits2) {
+  //   print(fruit);
+  // }
+  // List<int> numbers = [3, 1, 4, 1, 5, 9];
+  // numbers.sort((a, b) => b - a);
+  // print(numbers);
+  // List<String> names = ["Артем", "Мария", "Иван"];
+  // List<String> upper = names.map((name) => name.toUpperCase()).toList();
+  // print(upper);
+  // List<String> lonNames = names.where((name) => name.length > 4).toList();
+  // print(lonNames);
 }
