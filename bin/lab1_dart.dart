55db4c1f3a72175d34ef6830f7e394d7a95b5f78
YuriiSuperProg <uravasilev520@gmail.com>
2026-09-19 18:36:55 +0300

    learn dart cycles

--- bin/lab1_dart.dart
+++ bin/lab1_dart.dart
@@ -76,4 +76,40 @@ void main(List<String> arguments) {
   // print(upper);
   // List<String> lonNames = names.where((name) => name.length > 4).toList();
   // print(lonNames);
+  // int score2 = 85;
+  // String grade;
+  // if (score2 >= 90) {
+  //   grade = "A";
+  // } else if (score2 >= 75) {
+  //   grade = "B";
+  // } else {
+  //   grade = "C";
+  // }
+  // print(grade);
+  // String result = score2 >= 60 ? "Сдал" : "Не сдал";
+  // print(result);
+  // for (int i = 0; i < 5; i++) {
+  //   print(i);
+  // }
+  // List<String> fruits3 = ["Яблоко", "Банан", "Груша"];
+  // for (var fruit in fruits3) {
+  //   print(fruit);
+  // }
+  // int n = 0;
+  // while (n < 3) {
+  //   print(n);
+  //   n++;
+  // }
+  String day = "Пн";
+  switch (day) {
+    case "Сб":
+    case "Вс":
+      print("Выходной");
+      break;
+    case "Пн":
+      print("Начало недели");
+      break;
+    default:
+      print("Рабочий день");
+  }
 }
